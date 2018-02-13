60564802671a2a724d10ca7e7dd65b52b512ba31
Herryanto Siatono <herryanto@gmail.com>
2018-02-14 07:53:35 +0800

    Fix systemjs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
-import * as Circle from "./math/circle";
-import calc from "./math/rectangle";
+import * as Circle from './math/circle'
+import calc from './math/rectangle'
+import 'jQuery'
 
-console.log(Circle.PI);
-console.log(Circle.calculateCirumcumference(10));
-console.log(calc(20, 50));
+console.log(Circle.PI)
+console.log(Circle.calculateCirumcumference(10))
+console.log(calc(20, 50))
+
+// Change with JQuery
+$('app').css({ 'background-color': 'green' })
